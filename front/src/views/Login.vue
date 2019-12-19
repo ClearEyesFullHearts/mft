4762649428f672d3d0fc96f636f68b221879b72c
ClearEyesFullHearts <mathieu.FONT@amadeus.com>
2019-12-19 11:28:00 +0100

    pretty html

--- front/src/views/Login.vue
+++ front/src/views/Login.vue
@@ -5,21 +5,25 @@
         <div v-if="view !== 'register' && view !== 'reset'">
           <Login />
           <ul class="list-inline">
-            <li><a id="login-new-btn" class="list-inline-item" href="#" @click.prevent="view ='register'">Sign up!</a></li>
-            <li><a id="login-forgot-btn" class="list-inline-item" href="#" @click.prevent="view ='reset'">Forgot my password</a></li>
-        </ul>
+            <li><a id="login-new-btn" class="list-inline-item" href="#"
+                @click.prevent="view ='register'">Sign up!</a></li>
+            <li><a id="login-forgot-btn" class="list-inline-item" href="#"
+                @click.prevent="view ='reset'">Forgot my password</a></li>
+          </ul>
         </div>
         <div v-else-if="view === 'register'">
           <Register />
           <ul class="list-inline">
-            <li><a id="register-cancel-btn" class="list-inline-item" href="#" @click.prevent="view ='login'">Cancel</a></li>
-        </ul>
+            <li><a id="register-cancel-btn" class="list-inline-item" href="#"
+                @click.prevent="view ='login'">Cancel</a></li>
+          </ul>
         </div>
         <div v-else-if="view === 'reset'">
           <Reset />
           <ul class="list-inline">
-            <li><a id="reset-cancel-btn" class="list-inline-item" href="#" @click.prevent="view ='login'">Cancel</a></li>
-        </ul>
+            <li><a id="reset-cancel-btn" class="list-inline-item" href="#"
+                @click.prevent="view ='login'">Cancel</a></li>
+          </ul>
         </div>
       </transition>
     </div>
