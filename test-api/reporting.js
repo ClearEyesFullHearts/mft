0f5fd00c81d981da7d7017f4a2c6e51e63b3320f
ClearEyesFullHearts <mathieu.brick@gmail.com>
2020-01-16 18:12:26 +0100

    package version must be in a format

--- test-api/reporting.js
+++ test-api/reporting.js
@@ -3,13 +3,14 @@ var reporter = require('cucumber-html-reporter');
 let fileName = 'test-api-report';
 if(process.env.REPORT_NAME){
     fileName = process.env.REPORT_NAME;
+}
 
 var options = {
-        theme: 'bootstrap',
-        jsonFile: fileName + '.cucumber',
-        output: fileName + '.html',
-        reportSuiteAsScenarios: true,
-        scenarioTimestamp: true
-    };
- 
-    reporter.generate(options);
+    theme: 'bootstrap',
+    jsonFile: fileName + '.cucumber',
+    output: fileName + '.html',
+    reportSuiteAsScenarios: true,
+    scenarioTimestamp: true
+};
+
+reporter.generate(options);
