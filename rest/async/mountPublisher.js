eb3e0d05548ae2060ed7c12bc0312c912169657c
ClearEyesFullHearts <mathieu.brick@gmail.com>
2023-03-29 16:16:57 +0200

    clean up

--- rest/async/mountPublisher.js
+++ rest/async/mountPublisher.js
@@ -37,35 +37,14 @@ module.exports = async (app) => {
     connections: {}
   }
 
-  if(process.env.NODE_ENV === 'dev'){
-    debug('Set mock connections');
-    options.connections = {
-      rabbit: {
-        createChannel: () => ({
-          assertExchange: () => Promise.resolve(true),
-          assertQueue: () => Promise.resolve(true),
-          sendToQueue: (q, buffer, opts) => {
-            console.log('rabbit send message to queue');
-            console.log(q, buffer.toString(), opts);
-          },
-          publish: (exc, topic, buffer, opts) => {
-            console.log('rabbit publish message');
-            console.log(exc, topic, buffer.toString(), opts);
-          },
-        }),
-      },
-      garbage: {
-        send: (obj) => {
-          const { topic, messages } = obj;
-          console.log('garbage send message');
-          console.log(topic, messages);
-        }
-      }
-    }
-  } else {
+  if(process.env.NODE_ENV !== 'dev'){
+    debug('Connect to known servers');
     options.connections.rabbit = await getRabbitCon();
     options.connections.garbage = await getGarbageCon();
+  } else {
+    debug('Publisher will create the connections');
   }
+
   debug('Create publisher');
   const asyncMiddleware = await publish(asyncDoc, options);
   debug('Mount publisher on app');
