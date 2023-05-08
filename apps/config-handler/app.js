bedc7521ff42dba7cd77b382ac4724a765aed569
ClearEyesFullHearts <mathieu.brick@gmail.com>
2023-05-08 17:03:42 +0200

    load secret file in container

--- apps/config-handler/app.js
+++ apps/config-handler/app.js
@@ -1,5 +1,17 @@
-const ConfigHandler = require('./src/service');
+const fs = require('fs/promises');
 
 (async () => {
+  try {
+    const file = await fs.readFile('/run/secrets/external_config');
+    const secrets = JSON.parse(file);
+    Object.keys(secrets).forEach((k) => {
+      console.log('add to env', k);
+      process.env[k] = secrets[k];
+    });
+  } catch (err) {
+    console.log('No secrets', err);
+  }
+
+  const ConfigHandler = require('./src/service');
   await new ConfigHandler().start();
 })();
